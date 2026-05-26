15f99fd6eca641daff03b3973c21ef3a45d3d34f
Nikolay Stoykow <gitlab@stoykow.de>
2026-05-26 13:33:17 +0200

    ok

--- wokwi.ino
+++ wokwi.ino
@@ -5,43 +5,7 @@ void setup() {
 void loop() {
   // S
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(100);
+  delay(1000);
   digitalWrite(LED_BUILTIN, LOW);
-  delay(500);
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(100);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(500);
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(100);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(500);
-
-  // O
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(400);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(500);
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(400);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(500);
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(400);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(500);
-
-  // S
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(100);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(500);
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(100);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(500);
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(100);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(500);
+  delay(1000);
 }
